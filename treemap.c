2d7fcaa8729373aab4f7d1b122a7e2413e179d6d
Your Name <you@example.com>
2024-05-12 00:09:25 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -104,65 +104,6 @@ TreeNode * minimum(TreeNode * x){
 
 
 void removeNode(TreeMap * tree, TreeNode* node) {
-    // TreeNode* auxTemp = tree->root;
-    // TreeNode* auxParent = NULL;
-
-    // while( auxTemp != NULL && auxTemp != node){
-    //     auxParent = auxTemp;
-    //     if (node->pair->key < auxTemp->pair->key){
-    //         auxTemp = auxTemp->left;
-    //     } else {
-    //         auxTemp = auxTemp->right;
-    //     }
-    // }
-
-    // if (auxTemp == NULL){
-    //     return;
-    // }
-
-    // if (auxTemp->left == NULL && auxTemp->right == NULL){
-    //     if (auxTemp != tree->root){
-    //         if (auxParent->left == auxTemp){
-    //             auxParent->left = NULL;
-    //         }
-    //         else{
-    //             auxParent->right = NULL;
-    //         }
-    //     }
-    //     else{
-    //         tree->root = NULL;
-    //     }
-    //     free(auxTemp);
-    // }
-        
-    // else if (auxTemp->left == NULL || auxTemp->right == NULL){
-    //     TreeNode* child;
-
-    //     if (auxTemp->left != NULL){
-    //         child = auxTemp->left;
-    //     }
-    //     else{
-    //         child = auxTemp->left;
-    //     }
-    //     if (auxTemp != tree->root){
-    //         if (auxParent->left == auxTemp){
-    //             auxParent->left = child;
-    //         }
-    //         else{
-    //             auxParent->right = child;
-    //         }
-    //     }
-    //     else{
-    //         tree->root = child;
-    //     }
-    //     free(auxTemp);
-    // }
-    // else{
-    //     TreeNode* succesor = minimum(auxTemp->right);
-    //     eraseTreeMap(tree, succesor->pair->key);
-    //     auxTemp->pair->key = succesor->pair->key;
-    //     auxTemp->pair->value = succesor->pair->value;
-    // } 
     TreeNode* auxTemp = tree->root;
     TreeNode* auxParent = NULL;
 
